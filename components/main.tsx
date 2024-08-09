9ee00ed00d2a581a5291169765bcf2d6af90756e
JuanfeMonroyJ <felipemonroyjimenez@gmail.com>
2024-08-09 18:41:58 -0500

    modificando iconos, responsive y fonts

--- components/main.tsx
+++ components/main.tsx
@@ -10,30 +10,30 @@ const Main = () => {
        <Image
         src="/Imagen 1.png" 
         alt="Logo de Saras"
-        width={300}
-        height={100}
+        width={695}
+        height={665}
       />
 
       <Image
         src="/Bievenido-saras.png" 
         alt="Logo de Saras"
-        width={300}
-        height={100}
+        width={624.56}
+        height={465.05}
       />
         <Image
         src="/Ser-seguro.png" 
         alt="Logo ser seguro"
-        width={300}
-        height={100}
+        width={215}
+        height={154}
         />
        <Image
         src="/Boton-iniciar.png" 
         alt="Boton Iniciar Sesión"
-        width={300}
-        height={100}
+        width={255}
+        height={72}
         />
 
-      <a href="" className="underline">
+      <a href="" className="underline text-white">
         ¿No tienes cuenta? Regístrate
       </a>
 
