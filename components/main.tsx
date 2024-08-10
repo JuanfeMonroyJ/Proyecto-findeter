ac8b633e05ba69eff490cf45490e5036f0642897
JuanfeMonroyJ <felipemonroyjimenez@gmail.com>
2024-08-09 19:57:04 -0500

    Home Terminado

--- components/main.tsx
+++ components/main.tsx
@@ -3,44 +3,50 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 const Main = () => {
-    const router = useRouter();
-    return (
-        <main>
-       <div className="flex flex-col items-center justify-center min-h-screen">
-       <Image
-        src="/Imagen 1.png" 
-        alt="Logo de Saras"
-        width={695}
-        height={665}
-      />
+  const router = useRouter();
 
-      <Image
-        src="/Bievenido-saras.png" 
-        alt="Logo de Saras"
-        width={624.56}
-        height={465.05}
-      />
-        <Image
-        src="/Ser-seguro.png" 
-        alt="Logo ser seguro"
-        width={215}
-        height={154}
-        />
-       <Image
-        src="/Boton-iniciar.png" 
-        alt="Boton Iniciar Sesión"
-        width={255}
-        height={72}
-        />
+  return (
+    <main className="container-fluid flex items-center justify-center m-10">
+      <div className="flex items-center space-x-8">
+        <div>
+          <Image
+            src="/Imagen.png"
+            alt="Isometric office illustration"
+            width={695}
+            height={665}
+          />
+        </div>
 
-      <a href="" className="underline text-white">
-        ¿No tienes cuenta? Regístrate
-      </a>
-
-    </div>
+        <div className=" text-white flex flex-col items-center">
+          <Image
+            src="/intro.png"
+            alt="Saras introduction"
+            width={624.56}
+            height={465.05}
+          />
+          <div className="flex items-center space-x-4 mt-6 gap-32">
+            <Image
+              src="/Ser-seguro1.png"
+              alt="Logo ser seguro"
+              width={215}
+              height={154}
+            />
+            <div className="flex flex-col items-start space-y-4">
+              <button
+                className="bg-orange-400 text-white px-8 py-3 rounded-full text-xl font-semibold"
+                onClick={() => router.push("/login")}
+              >
+                Iniciar sesión
+              </button>
+              <a href="#" className="text-white underline">
+                ¿No tienes cuenta? Regístrate
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
-    );
-  };
-  
-  export default Main;
-  
+  );
+};
+
+export default Main;
