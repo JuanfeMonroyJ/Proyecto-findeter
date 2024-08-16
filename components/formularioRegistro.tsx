b6903b57f0667ffbfbddd194f823b58db0641462
JuanfeMonroyJ <felipemonroyjimenez@gmail.com>
2024-08-16 11:23:05 -0500

    components/ login/main/navbar/solicitud-terminados

---
+++ components/formularioRegistro.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+
+const  formularioRegistro= () => {
+    const router = useRouter();
+  
+    return (
+      <div className="container-fluid flex items-center justify-center m-10">
+        <div className="flex items-center space-x-8">
+          <div>
+            <Image
+              src="/Imagen.png"
+              alt="Isometric office illustration"
+              width={695}
+              height={665}
+            />
+          </div>
+  
+    
+            </div>
+          </div>
+      
+    );
+  };
+  
+  export default formularioRegistro;
+  
